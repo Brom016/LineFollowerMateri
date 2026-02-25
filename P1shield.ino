06cacfc9d34b575d2b2ef5aa0ddda2e3f562e006
Bromo <115420975+Brom016@users.noreply.github.com>
2026-02-25 14:48:36 +0700

    Update P1shield.ino

--- P1shield.ino
+++ P1shield.ino
@@ -1,8 +1,8 @@
 #define M1_PWM 11
 #define M1_DIR 12
 
-#define M2_PWM 3
-#define M2_DIR 13
+//#define M2_PWM 3
+//#define M2_DIR 13
 
 void setup() {
   Serial.begin(9600);
@@ -10,10 +10,8 @@ void setup() {
   pinMode(M1_PWM, OUTPUT);
   pinMode(M1_DIR, OUTPUT);
 
-  pinMode(M2_PWM, OUTPUT);
-  pinMode(M2_DIR, OUTPUT);
 
-  Serial.println("Test Motor M1 dan M2 dimulai...");
+  Serial.println("Test Motor M1 dimulai...");
 }
 
 void loop() {
@@ -21,27 +19,22 @@ void loop() {
   Serial.println("Motor MAJU");
 
   digitalWrite(M1_DIR, HIGH);
-  digitalWrite(M2_DIR, HIGH);
 
   analogWrite(M1_PWM, 200);
-  analogWrite(M2_PWM, 200);
 
   delay(3000);
 
   Serial.println("Motor STOP");
 
   analogWrite(M1_PWM, 0);
-  analogWrite(M2_PWM, 0);
 
   delay(2000);
 
   Serial.println("Motor MUNDUR");
 
   digitalWrite(M1_DIR, LOW);
-  digitalWrite(M2_DIR, LOW);
 
   analogWrite(M1_PWM, 200);
-  analogWrite(M2_PWM, 200);
 
   delay(3000);
 }
